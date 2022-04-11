31ca6fd59a3ac0dda97b95d02bea1e5ea45519d0
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2022-04-11 17:31:04 +0100

    dynamic programming: MaxSubsetSumsNoAdjacent

---
+++ Algorithms.Tests/MaxSubsetSumsNoAdjacentTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Algorithims.DynamicProgramming.Medium;
+
+namespace Algorithms.Tests
+{
+   public class MaxSubsetSumsNoAdjacentTests
+    {
+        [Fact]
+        public void ShouldReturn33() { 
+         //Arrrange
+         var array = new int[] { 7, 10, 12, 7, 9, 14 };
+
+            //Act 
+
+         var actual =   MaxSumsNoAdjacent.MaxSubsetSumNoAdjacent(array, false);
+         var expected = 33;
+       
+         //Assert
+         Assert.Equal(expected, actual);
+        
+        }
+
+        [Fact]
+        public void ShouldNotReturn33()
+        {
+            //Arrrange
+            var array = new int[] { 7, 10, 12, 7, 20, 14 };
+
+            //Act 
+
+            var actual = MaxSumsNoAdjacent.MaxSubsetSumNoAdjacent(array);
+            var expected = 33;
+
+            //Assert
+            Assert.NotEqual(expected, actual);
+
+        }
+
+
+    }
+}
