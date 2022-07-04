565c88bbe70fd9b0398986761307b5b4f6f8c3e6
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2022-07-04 18:55:27 +0100

    completed longest happy string problem

---
+++ Common/IntMaxComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class IntMaxComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+           return y.CompareTo(x);
+        }
+    }
+}
