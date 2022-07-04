3db60228996a18f7a5e4f2423830c5ce84cd3b73
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2022-07-04 17:15:47 +0100

    upgraded all libraries to .net 6, add clone graph solution

--- Common/BinaryTree.cs
+++ Common/BinaryTree.cs
@@ -22,4 +22,29 @@ namespace Common
             Value = value;
         }
     }
+
+
+    public class GraphNode
+    {
+        public int val;
+        public IList<GraphNode> neighbors;
+
+        public GraphNode()
+        {
+            val = 0;
+            neighbors = new List<GraphNode>();
+        }
+
+        public GraphNode(int _val)
+        {
+            val = _val;
+            neighbors = new List<GraphNode>();
+        }
+
+        public GraphNode(int _val, List<GraphNode> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
 }
---
+++ DataStructures/Graphs/Medium/CloneGraph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+
+namespace DataStructures.Graphs.Medium
+{
+    public class CloneGraph
+    {
+        public static GraphNode Clone(GraphNode node)
+        {
+            if (node is null)
+                return null;
+
+            var clonedDict = new Dictionary<GraphNode, GraphNode>();
+            var queue = new Queue<GraphNode>();
+
+            queue.Enqueue(node);
+            clonedDict.Add(node, new GraphNode(node.val));
+
+            while(queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+
+                foreach (var neigbhour in currentNode.neighbors)
+                {
+                    if(!clonedDict.ContainsKey(neigbhour))
+                    {
+                        clonedDict.Add(neigbhour, new GraphNode(neigbhour.val));
+                        queue.Enqueue(neigbhour);
+                    }
+
+                    clonedDict[currentNode].neighbors.Add(clonedDict[neigbhour]);
+
+                }
+            }
+        
+            return clonedDict[node];
+        }
+    }
+}
