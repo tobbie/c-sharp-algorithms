1bf542de3ca7960c9421fb0b9c55b8ae8e27303f
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2023-09-12 22:44:19 +0100

    fixed endless loop big in factorial class

---
+++ Common/WatchWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class WatchWrapper
+    {
+        public static long ElapsedTime {
+            get {
+                return Watch.ElapsedMilliseconds;
+            } 
+          }
+        private static Stopwatch Watch { get; set; }
+
+        public static void Start()
+        {
+            Watch = new Stopwatch();
+            Watch.Start();
+
+        }
+
+        public static void Stop()
+        {
+            Watch.Stop();
+        }
+    }
+}
