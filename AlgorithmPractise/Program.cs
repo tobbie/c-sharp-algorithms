541bf42633f7e4d1ccb77283257e60500a110f03
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2026-09-12 16:25:51 +0100

    added files to enable email notification

--- AlgorithmPractise/Program.cs
+++ AlgorithmPractise/Program.cs
@@ -1,11 +1,38 @@
 ﻿using Playground.BytesExamples;
+using Playground.EmailService;
+using System.Threading.Tasks;
+using static System.Console;
 
 namespace ProgramEntryPoint;
 
 public class Program
 {
-    public static void Main(string[] args)
+    //public static void Main(string[] args)
+    //{
+    //    ConvertTextToBytes.Run();
+    //}
+    public static async Task Main(string[] args)
     {
-        ConvertTextToBytes.Run();
+        var emailDelivery = new SesEmailDelivery();
+        var email = new EmailMessage
+        {
+            To = new[] { "", "" },
+            Subject = "SES Test Email 1",
+            Body = """
+                        <h1> Hello from Amazon SES! Part 1 </h1>
+                        <p> This is a test email from Tobi's C# application.</p>
+                        <p>You're doing well 1</P>
+                   """,
+            IsHtml = true
+        };
+
+     
+
+        WriteLine("Preparing to send email......");
+
+        await Task.Delay(2000);
+
+        await emailDelivery.SendAsync(email);
+   
     }
 }
---
+++ Playground/EmailService/SesEmailDelivery.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleEmailV2;
+using Amazon.SimpleEmailV2.Model;
+
+namespace Playground.EmailService;
+
+public sealed class SesEmailDelivery : IEmailDelivery
+{
+    private readonly IAmazonSimpleEmailServiceV2 _ses;
+    private readonly string _fromAddress;
+    private readonly string AccessKey = "";
+    private readonly string SecretKey = "";
+    public SesEmailDelivery()
+    {
+            var credentials =  new BasicAWSCredentials(AccessKey, SecretKey);
+            _ses = new AmazonSimpleEmailServiceV2Client(credentials, RegionEndpoint.EUWest1);
+           _fromAddress = "";
+    }
+    public async Task SendAsync(EmailMessage message, CancellationToken cancellationToken = default)
+    {
+        var request = new SendEmailRequest
+        {
+            FromEmailAddress = _fromAddress,
+            Destination = new Destination { ToAddresses = message.To.ToList() },
+            Content = new EmailContent
+            {
+                Simple = new Message
+                {
+                    Subject = new Content
+                    {
+                        Data = message.Subject,
+                    },
+
+                    Body = message.IsHtml ? new Body   {  Html = new Content {  Data = message.Body}  } // use html body
+                                          : new Body   {  Text = new Content {  Data = message.Body } }// otehrwise use text body
+                }
+            }
+
+        };
+
+        await _ses.SendEmailAsync(request, cancellationToken);
+    }
+}
